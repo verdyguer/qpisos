f5121ddd604ccc9e0be01e2b92064ae17b37e195
Mario Verdyguer <verdyguer@gmail.com>
2017-07-21 13:36:26 +0200

    login/out/singup

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express            = require('express');
+const path               = require('path');
+const favicon            = require('serve-favicon');
+const logger             = require('morgan');
+const cookieParser       = require('cookie-parser');
+const bodyParser         = require('body-parser');
+const expressLayouts     = require('express-ejs-layouts');
+const mongoose           = require('mongoose');
+const LocalStrategy      = require('passport-local').Strategy;
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const bcrypt             = require('bcrypt');
+const passport           = require('passport');
+const session            = require('express-session');
+const MongoStore         = require('connect-mongo')(session);
+const flash              = require('connect-flash');
 
-var app = express();
+mongoose.connect('mongodb://localhost:27017/qpisos');
 
+const index = require('./routes/index');
+const auth  = require('./routes/auth');
+const User     = require('./models/user');
+
+
+const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+app.use(expressLayouts);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,20 +35,106 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components/')))
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.js
+app.use(session({
+  secret: 'qpisossecret',
+  cookie: { maxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day 
+  })
+}));
+
+// NEW
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// Signing Up
+passport.use('local-signup', new LocalStrategy(
+  { passReqToCallback: true },
+  (req, username, password, next) => {
+    // To avoid race conditions
+    process.nextTick(() => {
+        User.findOne({
+            'username': username
+        }, (err, user) => {
+            if (err){ return next(err); }
+
+            if (user) {
+                return next(null, false);
+            } else {
+                // Destructure the body
+                const { username, email, description, password } = req.body;
+                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                const newUser = new User({
+                  username,
+                  email,
+                  description,
+                  password: hashPass
+                });
+
+                newUser.save((err) => {
+                    if (err){ next(err); }
+                    return next(null, newUser);
+                });
+            }
+        });
+    });
+}));
+//login
+passport.use('local-login', new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use( (req, res, next) => {
+  if (typeof(req.user) !== "undefined"){
+    res.locals.userSignedIn = true;
+  } else {
+    res.locals.userSignedIn = false;
+  }
+  next();
+});
+
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/', auth);
+// app.use('/campaigns', campaigns);
+// app.use('/',rewards);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email      : {type: String, require: true, unique: true},
+  password   : String,
+ }, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
