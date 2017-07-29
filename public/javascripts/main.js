e9b08c0776c6bdf0f26cb435426923a3c36da7ea
Mario Verdyguer <verdyguer@gmail.com>
2017-07-29 07:12:28 +0200

    localitation add no markers

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,26 @@
-$(document).ready(function(){
+ 
+$(document).ready(function () {
   const sol = {
     lat: 40.417080,
     lng: -3.703612
   };
 
-const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
     center: sol
   });
-   // Add listing markers to map
-  let markers = [];
-  myListings.forEach(function(listing){
-    let addres = listing. titlelisting.title
+ });
+
+  let marker = [];
+  myListings.forEach(function (listing) {
+    let title = "qpisos"
     let position = {
       lat: listing.location.coordinates[1],
       lng: listing.location.coordinates[0]
     };
-    var pin = new google.maps.Marker({ position, map, title  });
-    markers.push(pin)
+    var pin = new google.maps.Marker({ position, map, title });
+    marker.push(pin)
   });
 
-});
+
+
