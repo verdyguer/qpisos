6d76f8e85477336d3f384ed6125bcd996283c9ed
Mario Verdyguer <verdyguer@gmail.com>
2017-08-08 12:04:53 +0200

    shows

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -1,31 +1,12 @@
-$(document).ready(function(){
-
-	var titleTag = document.getElementById('listingName');
-	var url = 'http://localhost:3000/api/' + titleTag.dataset.id;
-
-	$.ajax({
-    url: url,
-    method: 'GET',
-    success: printMapAndMarker,
-    error: function(error) {
-    }
+function printMapAndMarker(title, lng, lat) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: { lat: lat, lng: lng }
   });
 
-  function printMapAndMarker(listing){
-  	var position = {
-  	  lat: listing.location.coordinates[1],
-  	  lng: listing.location.coordinates[0]
-  	};
-
-  	var map = new google.maps.Map(document.getElementById('map'), {
-  	  zoom: 15,
-  	  center: position
-  	});
-
-  	var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: listing.name
-    });
-  }
-});
+  var marker = new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map: map,
+    title: title
+  });
+}
