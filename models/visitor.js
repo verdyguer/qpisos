490cb7b9825966a7b7084b555e4e2b85fd25e295
Mario Verdyguer <verdyguer@gmail.com>
2017-08-25 21:23:38 +0200

    modal visitor

--- models/visitor.js
+++ models/visitor.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const visitorSchema = new Schema({
     visitorName: { type: String, require: true},
     visitorEmail: { type: String, require: true},
-    visitorPhone: { type: String, require: true}
+    visitorPhone: { type: String, require: true},
+    visitorDate:  {type: Date, default: Date.now },
+    visitorTime: {type: Date, default: Date.now }
+    
 });
 
 
--- routes/visitors.js
+++ routes/visitors.js
@@ -5,22 +5,29 @@ console.log ('entra modelo visitor')
 
 
 router.get('/new', (req, res, next) => {
-  console.log ('entra ruta visitor')
-  res.render('visitors/new')});
+  console.log ('entra ruta visitor/new')
+
+  res.render('visitors/new', {
+    visitorName: req.query.visitorName,
+    visitorTime: req.query.visitorTime,
+    visitorDate: req.query.visitorDate
+  
+  })});
 
 router.post('/', (req, res, next) => {
   const newVisitor = new visitor({
     visitorName: req.body.visitorName,
-    visitorPhone: req.body.visitorPhone,
-    visitorEmail: req.body.visitorEmail
+    visitorDate: req.body.visitorDate,
+    //visitorTime: req.body.visitorTime
  
   });
 console.log(newVisitor);
   newVisitor.save((err) => {
     if (err) {
-      res.render('/');
+      res.render('index');
     } else {
-      res.redirect('/visitors/new');
+      res.redirect('/visitors/new?visitorName=' + req.body.visitorName   + '     you are going to send an email asking for a visit on    '+  req.body.visitorDate + 'at  '+ req.body.visitorTime );
+  
     }
   });
 });
